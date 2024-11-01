1af01fbbb730eee2092822d3079ec4e44669a45f
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-01 12:10:53 +0100

    changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 export default function Layout() {
   return (
     <Tabs>
-         <Tabs.Screen name='index' />
-         <Tabs.Screen name='category' />
-         <Tabs.Screen name='search' />
-         <Tabs.Screen name='bookmarks' />
-         <Tabs.Screen name='profile' />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="compass" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="category"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="space-dashboard" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="search" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookmarks"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="bookmarks" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" size={20} color={color} />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
