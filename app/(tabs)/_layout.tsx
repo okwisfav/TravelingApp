fbd4eeb3cc0680c19de1ef486e35fe4ffd05a285
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-01 13:05:54 +0100

    ...

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,15 +2,38 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { Tabs } from 'expo-router';
 import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
 
 export default function Layout() {
   return (
-    <Tabs>
+    // for removing border 
+    <Tabs screenOptions={{
+       tabBarStyle: {
+        backgroundColor: Colors.bgColor,
+        borderTopWidth: 0,
+        padding: 0
+       },
+       
+       // removing text from the navabar and also adding color 
+       tabBarShowLabel: false,
+       tabBarActiveTintColor: Colors.black,
+       tabBarInactiveTintColor: '#999'
+    }}>
+
+      // for adding icons and also setting navbar 
       <Tabs.Screen
         name="index"
         options={{
           tabBarIcon: ({ color }) => (
-            <Ionicons name="compass" size={28} color={color} />
+            <View style={{
+              backgroundColor:Colors.primaryColor, 
+              paddingHorizontal:16,
+              paddingVertical:12,
+              borderRadius: 10,
+              height: 50,
+              }}>
+                 <Ionicons name="compass" size={28} color={color} />
+            </View>
           ),
         }}
       />
