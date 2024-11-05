72de0fefb6638da7bf15df17f9570e1e14b676ab
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-05 12:56:47 +0100

    ....

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,7 +1,9 @@
-import { View, Text } from 'react-native'
+import { View, Text, ListRenderItem, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { FlatList } from 'react-native-gesture-handler'
 import { StyleSheet } from 'react-native';
+import { ListingType } from '@/types/listingType';
+import Colors from '@/constants/Colors';
 
 
 type Props = {
@@ -10,32 +12,42 @@ type Props = {
 
 const Listings = ({listings}: Props) => {
 
-    const renderItems =({ item }) =>{
+    const renderItems:ListRenderItem<ListingType> =({ item }) =>{
         return(
-           <View>
-               <Text>{item.name}</Text>
-           </View>
+            <TouchableOpacity>
+                  <View style={styles.item}>
+                      <Image 
+                       source={{uri: item.image}}
+                        style={{styles.image}}
+                         />
+                  </View>
+            </TouchableOpacity>
         )
     }
   return (
     <View>
-        <FlatList data={listings} renderItem={renderItems} />
+        <FlatList data={listings} 
+        renderItem={renderItems} 
+        horizontal showsHorizontalScrollIndicator={false}/>
     </View>
   )
 }
 
 export default Listings
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    item:{
+        backgroundColor: Colors.white,
+        padding: 10,
+        borderRadius: 10,
+        marginRight: 20,
+        width: 220,
+    },
+    image: {
+        width:200,
+        height: 200,
+        borderRadius: 10,
+    }
+})
 
-interface RootObject {
-    id:string;
-    name: string;
-    image: string;
-    description: string;
-    rating: number;
-    price: string;
-    duration: string;
-    location: string;
-    category: string;
-}
+ 
